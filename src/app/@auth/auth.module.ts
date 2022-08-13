808ac56e441861b853c83f7ea83bdc80fc27a957
Anastasia <kdmnk.an@gmail.com>
2022-08-13 18:10:08 +0300

    feat: add auth routing

--- src/app/@auth/auth.module.ts
+++ src/app/@auth/auth.module.ts
@@ -1,19 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthRoutingModule } from 'src/app/@auth/auth-routing.module';
 import { AuthComponent } from 'src/app/@auth/auth.component';
 import { LoginComponent } from './components /login/login.component';
 import { RegisterComponent } from './components /register/register.component';
 
-
-
 @NgModule({
-  declarations: [
-    AuthComponent,
-    LoginComponent,
-    RegisterComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [AuthComponent, LoginComponent, RegisterComponent],
+  imports: [CommonModule, AuthRoutingModule],
 })
-export class AuthModule { }
+export class AuthModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SharedModule } from 'src/app/@shared/shared.module';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, SharedModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
