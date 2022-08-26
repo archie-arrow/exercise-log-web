8fd4404d81257a192655d1b6c4367065359394ce
Anastasia <kdmnk.an@gmail.com>
2022-08-26 17:14:22 +0300

    feat: remake spinner directive

--- src/app/@shared/directives/spinner.directive.ts
+++ src/app/@shared/directives/spinner.directive.ts
@@ -4,25 +4,20 @@ import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
   selector: '[appSpinner]',
 })
 export class SpinnerDirective {
-  spinner = this.renderer.createElement('i');
+  overlay = this.renderer.createElement('div');
+  spinner = this.renderer.createElement('div');
 
   constructor(private renderer: Renderer2, private elementRef: ElementRef) {
-    this.renderer.addClass(this.spinner, 'pi');
-    this.renderer.addClass(this.spinner, 'pi-spin');
-    this.renderer.addClass(this.spinner, 'pi-spinner');
-    this.renderer.addClass(this.spinner, 'hidden');
-    this.renderer.appendChild(this.elementRef.nativeElement, this.spinner);
+    this.renderer.addClass(this.spinner, 'loader');
+    this.renderer.addClass(this.overlay, 'loader-overlay');
+    this.renderer.appendChild(this.overlay, this.spinner);
   }
 
   @Input('appSpinner') set isLoading(loading: boolean | null) {
-    this.toggleSpinnerHidden(!!loading);
-  }
+    this.renderer.appendChild(this.elementRef.nativeElement, this.overlay);
 
-  private toggleSpinnerHidden(loading: boolean): void {
-    if (loading) {
-      this.renderer.removeClass(this.spinner, 'hidden');
-    } else {
-      this.renderer.addClass(this.spinner, 'hidden');
+    if (!loading) {
+      this.renderer.removeChild(this.elementRef.nativeElement, this.overlay);
     }
   }
 }
