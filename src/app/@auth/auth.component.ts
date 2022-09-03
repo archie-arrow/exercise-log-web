257276255ea7c4f24713fed55c5d54355a0268dc
Anastasia <kdmnk.an@gmail.com>
2022-09-03 15:57:05 +0300

    feat: create language dropdown

--- src/app/@auth/auth.component.ts
+++ src/app/@auth/auth.component.ts
@@ -6,6 +6,10 @@ import { SLIDER_ITEMS } from 'src/app/@auth/constants';
 import { AppState } from 'src/app/store/app.reducer';
 import { ResetAuthState } from 'src/app/store/auth/auth.actions';
 
+interface LanguageDropdownInterface {
+  name: string;
+}
+
 @UntilDestroy()
 @Component({
   selector: 'app-auth',
@@ -16,12 +20,18 @@ import { ResetAuthState } from 'src/app/store/auth/auth.actions';
 export class AuthComponent {
   sliderItems = SLIDER_ITEMS;
 
+  language: LanguageDropdownInterface[];
+
+  selectedLanguage!: LanguageDropdownInterface;
+
   constructor(private router: Router, private store: Store<AppState>) {
     this.router.events.pipe(untilDestroyed(this)).subscribe((event) => {
       if (event instanceof NavigationStart) {
         this.store.dispatch(ResetAuthState());
       }
     });
+
+    this.language = [{ name: 'ENG' }, { name: 'УКР' }];
   }
 
   backgroundImageValue(current: string): string {
--- src/app/@shared/shared.module.ts
+++ src/app/@shared/shared.module.ts
@@ -1,8 +1,9 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CarouselModule } from 'primeng/carousel';
+import { DropdownModule } from 'primeng/dropdown';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
@@ -21,6 +22,8 @@ import { SpinnerDirective } from './directives/spinner.directive';
     ReactiveFormsModule,
     HttpClientModule,
     SpinnerDirective,
+    DropdownModule,
+    FormsModule,
   ],
 })
 export class SharedModule {}
