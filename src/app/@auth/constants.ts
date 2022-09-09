b0c0fa71849d806354211b59eb02a752bae99f10
Anastasia <kdmnk.an@gmail.com>
2022-09-09 13:05:32 +0300

    feat: create all translations

--- src/app/@auth/constants.ts
+++ src/app/@auth/constants.ts
@@ -3,20 +3,20 @@ import { SlideInterface } from 'src/app/@auth/interfaces/slide.interface';
 export const SLIDER_ITEMS: SlideInterface[] = [
   {
     imgUrl: 'slider-item-1.jpg',
-    text: '“We often overestimate how often we do things, but tracking eliminates this weakness. When we track we just have the facts, not our subjective reflections.”',
-    name: 'Betsy Ramser Jaime',
-    job: 'Freelance Editor',
+    text: 'SLIDER.1.text',
+    name: 'SLIDER.1.name',
+    job: 'SLIDER.1.job',
   },
   {
     imgUrl: 'slider-item-2.jpg',
-    text: '“If we could give every individual the right amount of nourishment and exercise, not too little and not too much, we would have found the safest way to health.”',
-    name: 'Hippocrates',
-    job: 'Greek physician',
+    text: 'SLIDER.2.text',
+    name: 'SLIDER.2.name',
+    job: 'SLIDER.2.job',
   },
   {
     imgUrl: 'slider-item-3.jpg',
-    text: `“If you want to find the real competition, just look in the mirror. After awhile you'll see your rivals scrambling for second place.”`,
-    name: 'Criss Jam',
-    job: 'Musical artist',
+    text: 'SLIDER.3.text',
+    name: 'SLIDER.3.name',
+    job: 'SLIDER.3.job',
   },
 ];
